5961bc487cfa55abe7616050fc35ce19e5445859
cyroz1 <cyrz@pm.me>
2026-09-16 20:53:53 -0400

    Detect original HTTP scheme at edge

--- worker.js
+++ worker.js
@@ -2,12 +2,22 @@ const CANONICAL_HOST = "rollplay.cc";
 const WWW_HOST = `www.${CANONICAL_HOST}`;
 const LEGACY_PATHS = new Set(["/index", "/index/", "/index.html", "/index.php"]);
 
+function isHttpRequest(request, url) {
+  if (url.protocol === "http:") return true;
+
+  try {
+    return JSON.parse(request.headers.get("CF-Visitor") ?? "{}").scheme === "http";
+  } catch {
+    return false;
+  }
+}
+
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
     const needsCanonicalHost =
       url.hostname === WWW_HOST ||
-      (url.hostname === CANONICAL_HOST && url.protocol !== "https:");
+      (url.hostname === CANONICAL_HOST && isHttpRequest(request, url));
 
     if (needsCanonicalHost || LEGACY_PATHS.has(url.pathname)) {
       url.hostname = CANONICAL_HOST;
